6ed4806dc2f5cb15702c4c988d7c5cad8cd068f7
Colin Laws <laws.colin@uk.siak.com>
2019-05-29 13:01:26 +0100

    test commit

---
+++ Cleanup.psq.ps1
@@ -0,0 +1,27 @@
+. C:\Users\laws.colin\Documents\Git\ErrorHandling.ps1
+
+$usertemp=$env:TEMP
+$wintemp = $env:windir +"\temp"
+function DelFiles {
+    param (
+        [string]$path
+    )
+    $files=""
+    try {
+        $files = Get-ChildItem -path $path -File -Recurse -ErrorAction Stop
+    } catch {
+        $msg = "Failed to list files in " + $path
+        myerr -msg $msg
+    }
+    foreach ($file in $files){
+        try {
+            remove-Item -path $file.FullName -ErrorAction Stop
+        }catch{
+            # $_.exception
+            $msg = "Failed to delete " + $file.FullName
+            MyErr -msg $msg
+        }
+    }
+}
+DelFiles -path $usertemp
+DelFiles -path $wintemp
