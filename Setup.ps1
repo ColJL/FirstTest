eff09764b14cc5bfd1efb385c3e7e6a72129363e
Colin Laws <laws.colin@uk.siak.com>
2019-05-29 14:42:02 +0100

    Added removal of forcing edge

--- Setup.ps1
+++ Setup.ps1
@@ -5,8 +5,21 @@ $mydir = split-path $MyInvocation.InvocationName -Parent
 $mydir
 function CheckUpdates {
     $updatesession=(new-object -com "Microsoft.Update.Session")
-    $updates = $updatesession.CreateupdateSearcher().Search("IsInstalled=0 and IsHidden=0").Updates | Select-Object Title
+    $updates = $updatesession.CreateupdateSearcher().Search("IsInstalled=0 and IsHidden=0").Updates
     $updates.title
-    $updatesession.createDownloader
+    $updatesession.createupdatedownloader()
     read-host "Press enter to continue"
 }
+
+function NoEdge {
+    $layoutpath= ($Env:LOCALAPPDATA) + "\Microsoft\Windows\Shell\"
+    $layout = $layoutpath + "LayoutModification.xml"
+    $outfile = $layoutpath +"test.txt"
+    Get-Content $layout | Where-Object {($_ -notmatch 'Edge') -and ($_ -notmatch 'bing') -and ($_ -notmatch 'cortana')}  | Set-Content $outfile
+    remove-Item $layout
+    rename-item -force $outfile $layout
+    #Copy-Item -Force $layout C:\Users\Default\AppData\Local\Microsoft\Windows\Shell
+    Start-Process powershell -ArgumentList "\\welwms15\archive\programs\powershell\removedefaults.ps1" -verb runas
+}
+NoEdge
+CheckUpdates
